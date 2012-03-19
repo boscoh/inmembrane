23241bee0ce981110ff1d2b9ff7af7fcd17434cd
Bosco Ho <apposite@gmail.com>
2012-03-19 23:31:32 +1100

    Some data formatting in test_tmbhunt

--- tests/test_tmbhunt.py
+++ tests/test_tmbhunt.py
@@ -19,7 +19,28 @@ class TestTmbhunt(unittest.TestCase):
 
     self.params = inmembrane.get_params()
     self.params['fasta'] = "bomps.fasta"
-    self.expected_output = {'gi|107836852': {'tmbhunt_prob': 0.955956, 'tmbhunt': True}, 'gi|107837106': {'tmbhunt_prob': 0.23573599999999995, 'tmbhunt': False}, 'gi|107837101': {'tmbhunt_prob': 0.955956, 'tmbhunt': True}, 'gi|107836588': {'tmbhunt_prob': 0.903904, 'tmbhunt': True}, 'gi|107837107': {'tmbhunt_prob': 0.011001000000000039, 'tmbhunt': False}}
+    self.expected_output = {
+        'gi|107836852': {
+            'tmbhunt_prob': 0.955956, 
+            'tmbhunt': True
+        }, 
+        'gi|107837106': {
+            'tmbhunt_prob': 0.23573599999999995, 
+            'tmbhunt': False
+        }, 
+        'gi|107837101': {
+            'tmbhunt_prob': 0.955956, 
+            'tmbhunt': True
+        }, 
+        'gi|107836588': {
+            'tmbhunt_prob': 0.903904, 
+            'tmbhunt': True
+        }, 
+        'gi|107837107': {
+            'tmbhunt_prob': 0.011001000000000039, 
+            'tmbhunt': False
+        }
+    }
     self.prot_ids, \
     self.proteins = inmembrane.create_protein_data_structure(self.params['fasta'])
 
